4b9b28487ffeb7ba825de57a9098e534561d787f
Franziska <franzivoegele@web.de>
2019-11-06 14:34:49 +0100

    Ausnahmebehandlung, wenn keine Rezepte gefunden wurden, die der ZEit entsprechen.

--- src/page-filter/page-filter.js
+++ src/page-filter/page-filter.js
@@ -62,89 +62,93 @@ class PageFilter {
 
 
 //Mit Klick auf Button "Ergebnisse anzeigen, sollen die Werte die zuvor ausgewählt wurden ausgelesen werden (Basis, um später mit Werten aus Datenbank zu vergleichen)"
-async  _getValue(cbElements, kochzeitElement){
+    async  _getValue(cbElements, kochzeitElement){
 
-    //Datenbank-Objekt Erzeugung
-    let datenbank = new Database();
-    //Rezepte in rezepte-Array speichern
-    let rezepte = await datenbank.selectAllRezepte();
-    //Variablen-Deklaration
-    let minuten = kochzeitElement.innerHTML; //ausgewählte Kochzeit "holen"
-    let min = parseInt(minuten);
+        //Datenbank-Objekt Erzeugung
+        let datenbank = new Database();
+        //Rezepte in rezepte-Array speichern
+        let rezepte = await datenbank.selectAllRezepte();
+        //Variablen-Deklaration
+        let minuten = kochzeitElement.innerHTML; //ausgewählte Kochzeit "holen"
+        let min = parseInt(minuten);
 
 
-    //Definition von Funktionen, die zurückgeben, ob ein Rezept eine gewisse Kategorie erfüllt
-    function checkVegetarisch(rezept){
-    return rezept["vegetarisch"]==true;
-    }
-    function checkVegan(rezept){
-        return rezept["vegan"]==true;
-    }
-    function checkGlutenfrei(rezept){
-        return rezept["glutenfrei"]==true;
-    }
-    function checkLaktosefrei(rezept){
-        return rezept["laktosefrei"]==true;
-    }
+        //Definition von Funktionen, die zurückgeben, ob ein Rezept eine gewisse Kategorie erfüllt
+        function checkVegetarisch(rezept){
+        return rezept["vegetarisch"]==true;
+        }
+        function checkVegan(rezept){
+            return rezept["vegan"]==true;
+        }
+        function checkGlutenfrei(rezept){
+            return rezept["glutenfrei"]==true;
+        }
+        function checkLaktosefrei(rezept){
+            return rezept["laktosefrei"]==true;
+        }
 
-    //rezepte-Array auf das filtern, was Anwender im Filter ausgewählt hat an GErichtseigenschaften
-    if(cbElements[0].checked){
-        var veggirezepte = rezepte.filter(checkVegetarisch);
-    }else{
-        var veggirezepte=rezepte;
-    }
-    if(cbElements[1].checked){
-        var veganrezepte= veggirezepte.filter(checkVegan);
-    }else{
-        var veganrezepte=veggirezepte;
-    }
-    if(cbElements[2].checked){
-        var glutenfreirezepte= veganrezepte.filter(checkGlutenfrei);
-    }else{
-        var glutenfreirezepte=veganrezepte;
-    }
-    if(cbElements[3].checked){
-        var laktosefreirezepte= glutenfreirezepte.filter(checkLaktosefrei);
-    }else{
-        var laktosefreirezepte= glutenfreirezepte;
-    }
+        //rezepte-Array auf das filtern, was Anwender im Filter ausgewählt hat an GErichtseigenschaften
+        if(cbElements[0].checked){
+            var veggirezepte = rezepte.filter(checkVegetarisch);
+        }else{
+            var veggirezepte=rezepte;
+        }
+        if(cbElements[1].checked){
+            var veganrezepte= veggirezepte.filter(checkVegan);
+        }else{
+            var veganrezepte=veggirezepte;
+        }
+        if(cbElements[2].checked){
+            var glutenfreirezepte= veganrezepte.filter(checkGlutenfrei);
+        }else{
+            var glutenfreirezepte=veganrezepte;
+        }
+        if(cbElements[3].checked){
+            var laktosefreirezepte= glutenfreirezepte.filter(checkLaktosefrei);
+        }else{
+            var laktosefreirezepte= glutenfreirezepte;
+        }
 
-    //laktosefrei entspricht dem Stand dass nach allen vom Nutzer ausgewählten Checks gefiltert wurde.
-    //Bedeutet man kann in jedem Fall mit laktosefrei weiterarbeiten
-    //Rezepte aus der laktosefreirezepte-array auf page-filter ausgeben und dabei prüfen, ob die Zubereitungszeit des Rezepts aus der laktoserezepte-Array
-    //mit der angegebenen Zuberetungszeit aus der page-Filter übereinstimmt
-    if(min<=1){
-       alert("Die Zubereitungszeit ist kürzer als eine Minute.Überprüfen sie Ihre Zeitangabe");
-    }
+        //laktosefrei entspricht dem Stand dass nach allen vom Nutzer ausgewählten Checks gefiltert wurde.
+        //Bedeutet man kann in jedem Fall mit laktosefrei weiterarbeiten
+        //Rezepte aus der laktosefreirezepte-array auf page-filter ausgeben und dabei prüfen, ob die Zubereitungszeit des Rezepts aus der laktoserezepte-Array
+        //mit der angegebenen Zuberetungszeit aus der page-Filter übereinstimmt
+        if(min<=1){
+           alert("Die Zubereitungszeit ist kürzer als eine Minute.Überprüfen sie Ihre Zeitangabe");
+        }
+        let a=false;
+        for(let i=0; i<laktosefreirezepte.length; i++){
+              if(laktosefreirezepte[i]["zubereitungszeit"]<= min){
+                  let rezeptname=document.createElement("h4");
+                  rezeptname.innerHTML=laktosefreirezepte[i]["name"];
+                  document.getElementById("einblenden").appendChild(rezeptname);
+                  let a = true;
+              }
+        }//Ende for
+        if(!a){
+            alert("Es wurden leider keine Rezepte gefunden, die ihren Eingaben entsprechen.");
+        }
 
-    for(let i=0; i<laktosefreirezepte.length; i++){
-          if(laktosefreirezepte[i]["zubereitungszeit"]<= min){
-              let rezeptname=document.createElement("h4");
-              rezeptname.innerHTML=laktosefreirezepte[i]["name"];
-              document.getElementById("einblenden").appendChild(rezeptname);
-          }//Ende if
-    }//Ende for
+        //Beim Button-Click sollen die Elemente des filters ausgeblendet werden
+        document.getElementById("myCheckBox").style.display = "none";
+        document.getElementById("mySlider").style.display = "none";
+        document.getElementById("myResults").style.display = "none";
+        document.getElementById("i").style.display="none";
+        document.getElementById("name").style.display="none";
 
-    //Beim Button-Click sollen die Elemente des filters ausgeblendet werden
-    document.getElementById("myCheckBox").style.display = "none";
-    document.getElementById("mySlider").style.display = "none";
-    document.getElementById("myResults").style.display = "none";
-    document.getElementById("i").style.display="none";
-    document.getElementById("name").style.display="none";
+        //neuer bUtton wird erzeugt
+        let myBtn=document.createElement("button");
+        myBtn.classList.add('btn');
+        myBtn.innerHTML="Filter überarbeiten";
+        document.getElementById("hallo").appendChild(myBtn);
 
-    //neuer bUtton wird erzeugt
-    let myBtn=document.createElement("button");
-    myBtn.classList.add('btn');
-    myBtn.innerHTML="Filter überarbeiten";
-    document.getElementById("hallo").appendChild(myBtn);
+        //Beim Button-click auf myBtn, soll die page-Filter neu geladen werden
+        myBtn.addEventListener("click", () => this._einblenden());
+    }//Ende Funktion
 
-    //Beim Button-click auf myBtn, soll die page-Filter neu geladen werden
-    myBtn.addEventListener("click", () => this._einblenden());
-}//Ende Funktion
-
-//Methode zum Neuladen der page-filter Seite
-_einblenden(){
-location.reload();
-}//Ende Funktion
+    //Methode zum Neuladen der page-filter Seite
+    _einblenden(){
+    location.reload();
+    }//Ende Funktion
 
 }//Ende der Klasse
